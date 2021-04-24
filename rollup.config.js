3412e124c5001eb50a582dccc2019c05548d06de
sitek94 <msitkowski94@gmail.com>
2021-04-24 17:44:06 +0200

    feat: init rollup and typescript

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import clear from 'rollup-plugin-clear';
+import pkg from './package.json';
+
+const isProduction = process.env.PROD;
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    clear({
+      targets: ['dist'],
+    }),
+    typescript(),
+    isProduction && terser(),
+  ],
+};
---
+++ src/example/Example.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export function Example() {
+  return (
+    <div>
+      <h1>Example</h1>
+    </div>
+  );
+}
